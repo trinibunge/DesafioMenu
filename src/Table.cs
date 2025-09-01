b08ea5b23dae7d8c2adb2ca70432b198828c2833
Agustín Martínez <martinezagustin1215@gmail.com>
2025-09-01 11:30:28 -0300

    Clase Table

--- src/Table.cs
+++ src/Table.cs
@@ -8,10 +8,35 @@ namespace Ucu.Poo.Restaurant
     public class Table
     {
         private List<Dish> order = new List<Dish>();
+        public int Number { get; private set; }
+        public bool IsOccupied { get; private set; }
 
+        public Table(int number, bool isOccupied = false)
+        {
+            this.Number = number;
+            this.IsOccupied = isOccupied;
+        }
+        
         public bool HasOrders()
         {
             return this.order.Count > 0;
         }
+        
+        public void Occupy()
+        {
+            this.IsOccupied = true;
+        }
+
+        public void Free()
+        {
+            this.IsOccupied = false;
+            this.order.Clear();
+        }
+
+        public void AddToOrder(Dish dish)
+        {
+            this.order.Add(dish);
+        }
+
     }
 }

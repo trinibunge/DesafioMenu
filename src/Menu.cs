62cafb131454791115549ec437e3f5ba450b093a
paolabelen <paolabelen267@gmail.com>
2025-09-01 12:17:13 -0300

    implementación del menu

--- src/Menu.cs
+++ src/Menu.cs
@@ -9,5 +9,33 @@ namespace Ucu.Poo.Restaurant
     public class Menu
     {
         private List<Dish> dishes = new List<Dish>();
+        
+        public  void AddDish(Dish dish)
+        {
+            dishes.Add(dish);
+        }
+        public void RemoveDish(Dish dish)
+        {
+            dishes.Remove(dish);
+        }
+        public Dish GetDishByName(string plato)
+        {
+         
+
+            foreach (Dish dish in dishes)
+            {
+                if (dish.Name == plato)
+                { 
+                    return dish; 
+                }
+            }
+            return null;  
+        }
     }
 }
+
+
+    
+    
+    
+    
